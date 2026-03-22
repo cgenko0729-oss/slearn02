2bb14387e00579911bf6b5bbad6357986ecd9f9c
Genko1204019 <cgenko0729@gmail.com>
2026-03-22 13:32:21 +0900

    [Feature] Add PlayerController with basic movement

---
+++ Assets/01_Scripts/Player/PlayerControllerTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerTest : MonoBehaviour
+{   
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 720f;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
